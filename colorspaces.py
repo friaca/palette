a625dd966668cb0a18af0480cb692ab4a79b17db
friaca <v.fria@outlook.com>
2020-08-11 03:23:13 -0300

    [add/fix] encapsulating histogram generation and fixing `quantitize_image`

--- colorspaces.py
+++ colorspaces.py
@@ -1,5 +1,16 @@
-def quantitize_image(filename):
+from helper import run_command, delete_file
+
+def quantitize_image(filename, extension, colors):
   try:
-    run_command(f'magick {filename} -kmeans 5 {filename}_temp.png')
-  except:
+    run_command(f'magick {filename}.{extension} -kmeans {colors} {filename}_temp.png')
+  except Exception as e:
+    print('Something went wrong')
+
+def generate_histogram(filename):
+  try:
+    output = run_command(f'magick convert {filename} -format %c -depth 8 histogram:info:-', get_output=True)
+    delete_file(filename)
+
+    return output
+  except Exception as e:
     print('Something went wrong')
