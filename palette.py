28c824569a4fde455f5374b8a897a1fc532e4e6d
friaca <v.fria@outlook.com>
2020-08-11 03:39:06 -0300

    [add] encapsulating i/o functions on `try/except` block

--- palette.py
+++ palette.py
@@ -1,5 +1,5 @@
 import sys
-from helper import run_command
+from helper import run_command, delete_file
 from parse import parse_line, parse_args
 from colorspaces import quantitize_image, generate_histogram
 # from PIL import Image
@@ -7,22 +7,22 @@ from colorspaces import quantitize_image, generate_histogram
 def main():
   arguments = parse_args()
   filename, extension = arguments.input.rsplit('.', 1)
+  temp_file = f'{filename}_temp.png'
 
-  quantitize_image(filename, extension, arguments.colors)
-  imagick_output = generate_histogram(f'{filename}_temp.png')
+  try:
+    quantitize_image(filename, extension, arguments.colors)
+    imagick_output = generate_histogram(temp_file)
+    delete_file(temp_file)
+  except Exception:
+    print('Something went wrong')
+  else:
+    output_lines = imagick_output.split('\n')
+    parsed_lines = [parse_line(line) for line in output_lines]
+    sorted_lines = sorted(parsed_lines, key=lambda l: int(l['frequency']), reverse=True)
+    # image = Image.new("RGB", (1000, 1000), "#FF0000")
 
-  output_lines = imagick_output.split('\n')
-  parsed_lines = [parse_line(line) for line in output_lines]
-  sorted_lines = sorted(parsed_lines, key=lambda l: int(l['frequency']), reverse=True)
-  # image = Image.new("RGB", (1000, 1000), "#FF0000")
-
-  for line in sorted_lines:
-    print(line)
-  # image.show()
-
-
-
-
+    for line in sorted_lines:
+      print(line)
 
 if __name__ == '__main__':
   main()
