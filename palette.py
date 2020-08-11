0464143a08845acc65b5239f2b49d6275a2ac707
friaca <v.fria@outlook.com>
2020-08-11 03:25:02 -0300

    [rename] main.py to palette.py

--- palette.py
+++ palette.py
@@ -1,19 +1,23 @@
 import sys
+from helper import run_command
 from parse import parse_line, parse_args
-from helper import run_command, print_help
+from colorspaces import quantitize_image, generate_histogram
 # from PIL import Image
 
 def main():
-  parse_args(sys.argv)
-  return
-  imagick_output = run_command('magick convert group_less.jpg -format %c -depth 8 histogram:info:-', get_output=True)
+  arguments = parse_args()
+  filename, extension = arguments.input.rsplit('.', 1)
+
+  quantitize_image(filename, extension, arguments.colors)
+  imagick_output = generate_histogram(f'{filename}_temp.png')
+
   output_lines = imagick_output.split('\n')
   parsed_lines = [parse_line(line) for line in output_lines]
   sorted_lines = sorted(parsed_lines, key=lambda l: int(l['frequency']), reverse=True)
   # image = Image.new("RGB", (1000, 1000), "#FF0000")
 
-  for color in sorted_lines:
-    print(color)
+  for line in sorted_lines:
+    print(line)
   # image.show()
 
 
