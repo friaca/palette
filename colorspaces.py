9eb623f86fdd2328032a8085ddcd610b0cf3fdee
friaca <v.fria@outlook.com>
2020-08-13 20:45:31 -0300

    [change] cleaning exception expressions

--- colorspaces.py
+++ colorspaces.py
@@ -3,11 +3,11 @@ from helper import run_command
 def quantitize_image(filename, extension, colors):
   try:
     run_command(f'magick {filename}.{extension} -kmeans {colors} {filename}_temp.png')
-  except Exception as e:
+  except:
     print('Something went wrong')
 
 def generate_histogram(filename):
   try:
     return run_command(f'magick convert {filename} -format %c -depth 8 histogram:info:-', get_output=True)
-  except Exception as e:
+  except:
     print('Something went wrong')
