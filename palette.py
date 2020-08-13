66e92fafc417497367c2f3952ad31e74ba6a061e
friaca <v.fria@outlook.com>
2020-08-13 20:42:38 -0300

    [add] error handling on import and variable size

--- palette.py
+++ palette.py
@@ -2,7 +2,11 @@ import sys
 from helper import run_command, delete_file
 from parse import parse_line, parse_args
 from colorspaces import quantitize_image, generate_histogram
-from PIL import Image
+
+try:
+  from PIL import Image
+except ImportError:
+  print('Couldn\'t import Pillow, please intall it to use palette')
 
 def main():
   arguments = parse_args()
@@ -11,20 +15,22 @@ def main():
 
   try:
     quantitize_image(filename, extension, arguments.colors)
-    imagick_output = generate_histogram(temp_file)
+    imagick_output = generate_histogram(temp_file) 
     delete_file(temp_file)
   except:
-    print('Something went wrong')
+    print('Something went wrong with ImageMagick, is it installed correctly?')
   else:
     output_lines = imagick_output.split('\n')
     parsed_colors = [parse_line(line) for line in output_lines]
     sorted_colors = sorted(parsed_colors, key=lambda l: int(l['frequency']), reverse=True)
-    image = Image.new("RGB", (1000, 1000), "#FFFFFF")
+
+    size = 1000 if arguments.colors < 6 else arguments.colors * 200 
+    image = Image.new("RGB", (size, size), "#FFFFFF")
 
     for i in range(len(sorted_colors)):
-      initialX = (1000 // arguments.colors) * i
-      finalX = (1000 // arguments.colors) * (i + 1)
-      image.paste(sorted_colors[i]['hex'], [initialX, 0, finalX, 1000])
+      initialX = (size // arguments.colors) * i
+      finalX = (size //  arguments.colors) * (i + 1)
+      image.paste(sorted_colors[i]['hex'], [initialX, 0, finalX, size])
 
     image.show()
 
