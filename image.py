a3070424e39e4d65b8e7ee96af85ad1e6301f5bb
friaca <v.fria@outlook.com>
2020-08-19 01:07:03 -0300

    [change] trying to simplify the palette generation a bit

--- image.py
+++ image.py
@@ -1,45 +1,52 @@
-from colorspaces import get_contrasting_color
+from colorspaces import get_contrasting_color, get_color_indices
 
 try:
   from PIL import Image, ImageFont, ImageDraw
 except ImportError:
   print('Couldn\'t import Pillow, please intall it to use palette')
 
-def without_source(size, n_color, with_values, color_list):
-  image = Image.new("RGB", (size, size), "#FFFFFF")
 
-  for i in range(len(color_list)):
-    initialX = (size // n_color) * i
-    finalX = (size //  n_color) * (i + 1)
-    image.paste(color_list[i]['hex'], [initialX, 0, finalX, size])
+def generate_palette(color_list, show_values, by_percentage):
+  size = 1000 if len(color_list) < 6 else len(color_list) * 200
+  new_image = Image.new("RGB", (size, size), "#FFFFFF")
+  color_indices = get_color_indices(color_list, size, by_percentage)
+  n_color = len(color_indices)
 
-  if with_values:
-    drawing = ImageDraw.Draw(image)
+  for i in range(len(color_indices)):
+    new_image.paste(color_indices[i]['hex'], [color_indices[i]['start'], 0, color_indices[i]['end'], size])
+
+  if show_values and not by_percentage:
+    drawing = ImageDraw.Draw(new_image)
     font = ImageFont.truetype("arial", 30)
 
-    for i in range(len(color_list)):
+    for i in range(n_color):
       fill = get_contrasting_color(color_list[i]['hex'])
       x = ((size // n_color) * i) + 30
       
       drawing.text((x, size - 75), color_list[i]['hex'], font=font, fill=fill)
 
-  return image
+  return new_image
 
 def with_source(source_path, n_color, color_list):
   source_image = Image.open(source_path)
+  pos = (0, 0)
   size = (0, 0)
+  thumbnail_size = (0, 0)
   
   if source_image.size[0] > source_image.size[1]:
-    size = (1500, 1000)
-  elif source_image.size[0] < source_image.size[1]:
+    size = (1200, 1000)
+    thumbnail_size = (1000, 1000)
+    # pos = ((size[0] // 2) - (thumbnail_size[0]), size[1] - thumbnail_size[1])
+    pos = (125, 125)
+    print(pos)
+  elif source_image.size[0] > source_image.size[1] or source_image.size[0] == source_image.size[1]:
     size = (1000, 1500)
-  else:
-    pass
+    thumbnail_size = (750, 500)
   
 
   image = Image.new("RGB", size, "#FFFFFF")
-  source_image.thumbnail((750, 500), Image.ANTIALIAS)
+  source_image.thumbnail(thumbnail_size, Image.ANTIALIAS)
 
-  image.paste(source_image, (150, 150))
+  image.paste(source_image, pos)
 
   return image
--- palette.py
+++ palette.py
@@ -2,7 +2,7 @@ import sys
 from helper import run_command, delete_file
 from parse import parse_line, parse_args
 from colorspaces import quantitize_image, generate_histogram
-from image import without_source
+from image import without_source, with_source, generate_palette
 
 def main():
   arguments = parse_args()
@@ -20,18 +20,11 @@ def main():
     parsed_colors = [parse_line(line) for line in output_lines]
     sorted_colors = sorted(parsed_colors, key=lambda l: int(l['frequency']), reverse=True)
 
-    size = None
-    image = None
-    
-    if arguments.source:
-      pass
-    else:
-      size = 1000 if arguments.colors < 6 else arguments.colors * 200
-      image = without_source(size, arguments.colors, arguments.value, sorted_colors)
+    image = generate_palette(sorted_colors, arguments.values, arguments.percentage)
 
     try:
       output, extension = arguments.output.rsplit('.', 1)
-      image.save(f'{output}_palette.{extension}')
+      image.save(f'{output}.{extension}')
     except OSError as oserror:
       print(f'Couldn\'t save the image for some reason\n{oserrror.strerror}') 
 
